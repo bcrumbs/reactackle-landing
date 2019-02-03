d10f32a30aa7baae71010af169e6271a724a0ae4
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-03 18:39:29 +0000

    Deploy reactackle.braincrumbs.io to github.com/bcrumbs/reactackle-landing.git:gh-pages

---
+++ precache-manifest.036f1c3ca4a45411f0f8068fc87c19f7.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "84b948bf119e16016cb7",
+    "url": "/static/js/main.84b948bf.chunk.js"
+  },
+  {
+    "revision": "816f96cddfaf54918982",
+    "url": "/static/js/1.816f96cd.chunk.js"
+  },
+  {
+    "revision": "c6604ad02d14356f4942be01c7ab897d",
+    "url": "/index.html"
+  }
+];
